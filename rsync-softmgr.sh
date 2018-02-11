e7bb9f96d66cf70809a081b5cc3ff9d6fd69e950
zhangjianfei <zhangjianfei@360.net>
2018-02-11 18:22:09 +0800

    modify

--- rsync-softmgr.sh
+++ rsync-softmgr.sh
@@ -1,4 +1,3 @@
 while inotifywait -r -e modify,create,delete /home/vagrant/code/ent_app_store_source/web/app/controllers; do
-    /usr/bin/rsync -ratlz --rsh="/usr/bin/sshpass -p \"123!@#qaz\" ssh -o StrictHostKeyChecking=no -l root" /home/vagrant/code/ent_app_store_source/web/app/controllers root@10.98.2.159:/data/opt/docker/www/app/controllers
-    
+    /usr/bin/rsync -avz  /home/vagrant/code/ent_app_store_source/web/app/controllers root@10.98.2.159:/data/opt/docker/www/app/controllers
 done
